b65c9cbb1d1b79ed6a266068739dd5e4fcef6376
Bazarsukh <134251218+Bazarsukh@users.noreply.github.com>
2023-05-22 14:39:38 +0800

    Add files via upload

---
+++ lab7/facee.js
@@ -0,0 +1,8 @@
+document.getElementById("btnLogin").addEventListener("click", function() {
+    var form = document.getElementById("myForm");
+    if (form.style.display === "none") {
+      form.style.display = "block";
+    } else {
+      form.style.display = "none";
+    }
+  });
